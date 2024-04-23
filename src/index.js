706750e7fdbfd44b753112fb1dab36ed1b8e5d5b
VietnamNeko <58660787+VietnamNeko@users.noreply.github.com>
2024-04-23 11:10:39 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-const Express = require('express');
-require('dotenv').config();
+const Express = require("express");
+require("dotenv").config();
 
 const app = Express();
-
+let responsesGiven = 0;
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: false }));
 
-app.post('/check', function(req, res, next) {
-    console.log(req)
-    res.send('{"response": "OK"}');
+app.post("/check", function (req, res, next) {
+  console.log(req.body);
+  //let data = JSON.parse(req.body.content);
+  //console.log(data.a);
+  responsesGiven++;
+  res.send({"response":"n°"+responsesGiven});
 });
 
-app.listen('2001', function() {
-    console.log('listening on port 2001');
+app.listen(process.env.PORT || 2000, function () {
+  console.log("listening on port" + process.env.PORT || 2000);
 });
